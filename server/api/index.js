cc4103952db62e8581ab95e1ea7e49007ea48629
Stefani Chang <stefymerayja@gmail.com>
2023-05-05 12:30:02 -0400

    backend: added routes to index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,12 @@
+const { route } = require('./users');
+
 const router = require('express').Router();
 
 router.use('/users', require('./users'));
 router.use('/authentication/auth', require('./authentication/auth'));
+router.use('./posts', require('./posts'));
+router.use('/restaurants', require('./restaurants'));
+router.use('/reviews', require('/reviews'));
 
 router.use(function (req, res, next) {
   const err = new Error('Cannot find API route!');
