e51d5a7671de2a2bc1afef14280758929a6fc420
reginar-kim <kimregina.r@gmail.com>
2023-05-06 12:34:49 -0400

    Updated leaderboard component code

--- client/components/Leaderboard/Leaderboard.js
+++ client/components/Leaderboard/Leaderboard.js
@@ -3,39 +3,49 @@
 
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getLeaderboard } from '../redux/actions/getLeaderboard';
+import { fetchLeaderboard } from '../../redux/actions/leaderboardActions';
 
 const Leaderboard = () => {
-  const dispatch = useDispatch();
-  const leaderboardData = useSelector((state) => state.leaderboard.leaderboardData);
-
-  useEffect(() => {
-    dispatch(getLeaderboard());
-  }, [dispatch]);
-
-  return (
-    <div>
-      <h1>Leaderboard</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>User</th>
-            <th>Restaurant Visits</th>
-          </tr>
-        </thead>
-        <tbody>
-          {leaderboardData.map((user, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.restaurantVisitCount}</td>
+    const dispatch = useDispatch();
+    const leaderboard = useSelector(state => state.leaderboard.leaderboard);
+    const loading = useSelector(state => state.leaderboard.loading);
+    const error = useSelector(state => state.leaderboard.error);
+  
+    useEffect(() => {
+      dispatch(fetchLeaderboard());
+    }, [dispatch]);
+  
+    if (loading) {
+      return <div>Loading leaderboard...</div>;
+    }
+  
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+  
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>User</th>
+              <th>Restaurant Visits</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default Leaderboard;
+          </thead>
+          <tbody>
+            {leaderboard.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.restaurantVisitCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default Leaderboard;
