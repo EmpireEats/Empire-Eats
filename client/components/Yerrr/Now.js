5707cb163cc67d1b09f1eda443a913dec43f1c64
marlisajp <marlisajp@gmail.com>
2023-05-06 18:21:56 -0400

    yerr-now tab displaying posts with pag

--- client/components/Yerrr/Now.js
+++ client/components/Yerrr/Now.js
@@ -1,34 +1,68 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from 'react';
 import '../../../public/styles/now.css';
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllPostsAsync } from '../../redux/actions/postActions';
 
 const Now = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.post.allPosts);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 2;
+
+  useEffect(() => {
+    dispatch(fetchAllPostsAsync());
+  }, []);
+
+  // pagination
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   const handleNavigate = () => {
-    navigate('/chat');
+    navigate('/yerrr/chat');
   };
 
   return (
     <div className='user-post-list'>
-      <div className='user-post'>
-        <p>User: Alice</p>
-        <p>Post Description: "Going to eat at KPot!"</p>
-        <p>1 on 1</p>
-        <button onClick={handleNavigate}>👍🏽</button>
-        <span>
-          <button>👎🏽</button>
-        </span>
-      </div>
-      <div className='user-post'>
-        <p>User: John</p>
-        <p>Post Description: "Going to get sushi!"</p>
-        <p>Group</p>
-        <button onClick={handleNavigate}>👍🏽</button>
-        <span>
-          <button>👎🏽</button>
-        </span>
-      </div>
+      {posts && (
+        <>
+          <div className='user-post'>
+            {currentPosts.map((post) => (
+              <div key={`${post.userId}-${post.id}`} className='user-post'>
+                <p>User: {post.user.firstName}</p>
+                <p>Preference: {post.preference}</p>
+                {post.isActive ? <p>Active</p> : <p>No Longer Active</p>}
+                <button onClick={handleNavigate}>👍🏽</button>
+                <span>
+                  <button>👎🏽</button>
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className='pagination'>
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}>
+              Previous
+            </button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}>
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- client/redux/store/configureStore.js
+++ client/redux/store/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../reducers/authReducer';
 import yerrrChatReducer from '../reducers/yerrrChatReducers';
+import postReducer from '../reducers/postReducer';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    yerrrChat: yerrrChatReducer, 
+    yerrrChat: yerrrChatReducer,
+    post: postReducer,
   },
 });
 
