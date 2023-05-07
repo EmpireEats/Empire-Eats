174fde47121a57463571f48d481d7e36461784b1
Mary Stephanie Martinez <msamartinez1@gmail.com>
2023-05-07 17:58:20 -0400

    updated navbar to include other links

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -7,6 +7,10 @@ const NavBar = () => {
       <Link to='/login'>Login</Link>
       <Link to='/signup'>Sign Up</Link>
       <Link to='/'>⬅</Link>
+      <Link to='/yerrr/now'>YERRR</Link>
+      <Link to='/users/profile'>👤</Link>
+      <Link to='/leaderboard'>Leaderboard</Link>
+      <Link to='/map'>Map</Link>
     </div>
   );
 };
