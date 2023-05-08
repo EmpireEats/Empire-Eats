378ad8431a031600c8953bcb6897a0362f3c0065
marlisajp <marlisajp@gmail.com>
2023-05-08 16:26:59 -0400

    added chatmemberId async

--- client/redux/reducers/postReducer.js
+++ client/redux/reducers/postReducer.js
@@ -4,12 +4,14 @@ import {
   addPostAsync,
   deletePostAsync,
 } from '../actions/postActions';
+import { fetchChatMembersIdAsync } from '../actions/userInteractionActions';
 
 export const postSlice = createSlice({
   name: 'post',
   initialState: {
     allPosts: [],
     singleUsersPosts: [],
+    chatMemberIds: [],
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -26,6 +28,10 @@ export const postSlice = createSlice({
           (post) => post.id === postId
         );
         state.allPosts.splice(findPostIndex, 1);
+      })
+      .addCase(fetchChatMembersIdAsync.fulfilled, (state, action) => {
+        console.log('Updating chat member IDs in state:', action.payload);
+        state.chatMemberIds = action.payload;
       });
   },
 });
