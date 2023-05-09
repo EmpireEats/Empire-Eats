7a576f203010b230f2d08ae03238c8e0d0913888
Stefani Chang <stefymerayja@gmail.com>
2023-05-09 12:05:58 -0400

    only works when you replace api key

--- client/components/Restaurants/Map.js
+++ client/components/Restaurants/Map.js
@@ -1,22 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { LoadScript } from '@react-google-maps/api';
 
 const Map = () => {
+  const [latitude, setLatitude] = useState(40.7128);
+  const [longitude, setLongitude] = useState(-74.0060);
+  const mapRef = useRef();
+
   const defaultProps = {
-    center: { lat: 40.7128, lng: -74.0060 },
+    center: { lat: latitude, lng: longitude },
     zoom: 11
   };
 
   return (
-    <div style={{ height: '50vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyB8WHeAkLekUORmNa6_J30MwviZqj6qMM8' }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      ></GoogleMapReact>
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}
+      libraries={['places']}
+    >
+      <div style={{ height: "50vh", width: "100vh" }}>
+        <GoogleMapReact
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => {
+            mapRef.current = { map, maps };
 
-    </div>
-  );
+            if (maps && maps.Geolocation) {
+              const geolocation = maps.Geolocation;
+              geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                setLatitude(latitude);
+                setLongitude(longitude);
+                map.setCenter(new maps.LatLng(latitude, longitude));
+                // ...
+              });
+            }
+          }}
+        >
+          {/* Render markers or other map components */}
+        </GoogleMapReact>
+      </div>
+    </LoadScript>
+  );  
 };
 
 export default Map;
