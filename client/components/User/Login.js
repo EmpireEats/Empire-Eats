aa8c12913edf32ead7f5316dd48261b86582c9fb
Mary Stephanie Martinez <msamartinez1@gmail.com>
2023-05-08 23:00:47 -0400

    improved login form usability and UX

--- client/components/User/Login.js
+++ client/components/User/Login.js
@@ -1,39 +1,38 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { login, logout } from '../../redux/actions/authActions';
-import { useNavigate } from 'react-router'; // 1st change
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../redux/actions/authActions';
+import { useNavigate } from 'react-router';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
-  const navigate = useNavigate(); // 2nd change
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     dispatch(login({ email, password }));
-    navigate('/yerrr'); // 3rd change
-  };
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
+    navigate('/yerrr');
   };
 
   return (
     <form>
       <input
         type='email'
+        placeholder='Email'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type='password'
+        placeholder='Password'
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+      {isLoggedIn ? null : (
+        <button onClick={handleLogin}>Login</button>
+      )}
     </form>
   );
 };
