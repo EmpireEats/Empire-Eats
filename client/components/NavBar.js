eeacd415d9d07db33716f0f6d76b4be108615d95
Mary Stephanie Martinez <msamartinez1@gmail.com>
2023-05-08 22:11:10 -0400

    updated navbar based on user auth status

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../redux/actions/authActions';
 
 const NavBar = () => {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+    dispatch(logout());
+  };
+
   return (
     <div className='nav-container'>
-      <Link to='/login'>Login</Link>
-      <Link to='/signup'>Sign Up</Link>
-      <Link to='/'>⬅</Link>
-      <Link to='/yerrr/now'>YERRR</Link>
-      <Link to='/users/profile'>👤</Link>
-      <Link to='/leaderboard'>Leaderboard</Link>
-      <Link to='/restaurants'>Map</Link>
+      {auth.token ? (
+        <>
+          <Link to='/' onClick={handleLogout}>Logout</Link>
+          <Link to='/yerrr/now'>YERRR</Link>
+          <Link to='/restaurants'>We Outside</Link>
+          <Link to='/leaderboard'>Leaderboard</Link>
+          <Link to='/users/:id'>👤</Link>
+        </>
+      ) : (
+        <>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Sign Up</Link>
+        </>
+      )}
     </div>
   );
 };
