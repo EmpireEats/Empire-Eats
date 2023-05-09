34aa9b086fcb5c1ddb6410884dc45be52b74d0dd
reginar-kim <kimregina.r@gmail.com>
2023-05-09 01:36:02 -0400

    Added the reviews and photos route in the leaderboard

--- server/api/leaderboard.js
+++ server/api/leaderboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Leaderboard = require('../db/models/Leaderboard');
 
-
+// GET /api/leaderboard
 router.get('/', async (req, res) => {
   try {
     const leaderboard = await Leaderboard.getLeaderboard();
@@ -12,11 +12,32 @@ router.get('/', async (req, res) => {
   }
 });
 
+// PUT /api/leaderboard/update
 router.put('/update', async (req, res) => {
   const userData = req.body;
   try {
     await Leaderboard.updateUserRankings(userData);
     res.status(200).json({ message: 'User rankings updated' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET /api/leaderboard/reviews
+router.get('/reviews', async (req, res) => {
+  try {
+    const reviews = await Review.getRecentReviews();
+    res.json(reviews);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get recent photos
+router.get('/photos', async (req, res) => {
+  try {
+    const photos = await Photo.getRecentPhotos();
+    res.json(photos);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
