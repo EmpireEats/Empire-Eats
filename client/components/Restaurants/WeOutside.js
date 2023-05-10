53f5ee8d3803c227e26f9bd0d4a4433876e38952
Stefani Chang <stefymerayja@gmail.com>
2023-05-09 22:29:40 -0400

    renders a list with review buttons

--- client/components/Restaurants/WeOutside.js
+++ client/components/Restaurants/WeOutside.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRestaurants } from '../../redux/actions/restaurantActions';
 import Map from './Map';
-import RestaurantsList from './RestaurantsList';
+import List from './List';
 
 const WeOutside = () => {
-  return(
+  const dispatch = useDispatch();
+  const { allRestaurants } = useSelector(state => state.restaurant);
+  const [isMapsLoaded, setIsMapsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchRestaurants());
+    const apiKey = 'AIzaSyB8WHeAkLekUORmNa6_J30MwviZqj6qMM8';
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+    script.onload = () => setIsMapsLoaded(true);
+    document.body.appendChild(script);
+  }, [dispatch]);
+
+  return (
     <div>
-      <Map />
-      <RestaurantsList />
+      {isMapsLoaded && <Map restaurants={allRestaurants} />}
+      <List restaurants={allRestaurants} />
     </div>
-  )
+  );
 };
 
 export default WeOutside;
