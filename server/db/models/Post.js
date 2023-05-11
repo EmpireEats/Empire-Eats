8c0bedfd1c32464706af38c2788b5b96588dd362
marlisajp <marlisajp@gmail.com>
2023-05-11 19:28:46 -0400

    added message to post model & changed dummydata

--- server/db/models/Post.js
+++ server/db/models/Post.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Post = db.define('post', {
+  message: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   preference: {
     type: Sequelize.ENUM('one on one', 'group', 'no preference'),
     allowNull: false,
