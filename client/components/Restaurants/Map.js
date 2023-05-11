d2055263f82f445e4f84c7cdf0c5f14e10f61b39
Stefani Chang <stefymerayja@gmail.com>
2023-05-10 23:50:21 -0400

    list renders/map renders

--- client/components/Restaurants/Map.js
+++ client/components/Restaurants/Map.js
@@ -1,37 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import { getGeolocation } from '../../redux/actions/restaurantActions';
+
+let map;
+const NYC_BOUNDS = {
+  north: 40.917577,
+  south: 40.477399,
+  west: -74.25909,
+  east: -73.700181,
+};
+const DEFAULT_CENTER = { lat: 40.712776, lng: -74.005974 };
 
 const Map = ({ restaurants }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [map, setMap] = useState(null);
 
   useEffect(() => {
-    // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     const script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB8WHeAkLekUORmNa6_J30MwviZqj6qMM8';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB8WHeAkLekUORmNa6_J30MwviZqj6qMM8&callback=initMap';
+    document.head.appendChild(script);
     script.onload = () => setIsLoaded(true);
-    document.body.appendChild(script);
-  }, []);
 
-  const initMap = async () => {
-    const { Map } = await window.google.maps.importLibrary('maps');
-    const mapElement = document.getElementById('map');
-    const options = {
-      center: { lat: 40.712776, lng: -74.005974 }, // Default to NYC
-      zoom: 12,
+    return () => {
+      document.head.removeChild(script);
     };
-    const newMap = new Map(mapElement, options);
-    setMap(newMap);
-  };
+  }, []);
 
   useEffect(() => {
     if (isLoaded) {
-      initMap();
+      getGeolocation()
+        .then(({ latitude, longitude }) => {
+          const center = { lat: latitude, lng: longitude };
+          const options = {
+            center,
+            restriction: {
+              latLngBounds: NYC_BOUNDS,
+              strictBounds: true,
+            },
+            zoom: 14,
+          };
+          map = new window.google.maps.Map(document.getElementById('map'), options);
+        })
+        .catch(error => {
+          console.error(error);
+          const options = {
+            center: DEFAULT_CENTER,
+            restriction: {
+              latLngBounds: NYC_BOUNDS,
+              strictBounds: true,
+            },
+            zoom: 14,
+          };
+          map = new window.google.maps.Map(document.getElementById('map'), options);
+        });
     }
   }, [isLoaded]);
 
   return (
     <div>
-      <div id="map" style={{ height: '500px', width: '100%' }} />
+      <div id="map" style={{ height: '300px', width: '100%' }} />
     </div>
   );
 };
--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -1,47 +1,59 @@
-require('dotenv').config();
 const express = require('express');
-const router = express.Router();
 const axios = require('axios');
+const router = express.Router();
+
+const nycBounds = {
+  north: 40.917577,
+  south: 40.477399,
+  east: -73.700272,
+  west: -74.259090
+};
 
 router.get('/', async (req, res) => {
   try {
-    const { latitude, longitude } = req.query;
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=${apiKey}`);
-    const results = response.data.results;
-    const restaurants = results.map(result => {
-      return {
-        name: result.name,
-        address: result.vicinity,
-        rating: result.rating,
-        photoUrl: result.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=${apiKey}` : null,
-        placeId: result.place_id
-      };
-    });
+    const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.latitude},${req.query.longitude}&radius=1609.34&type=restaurant&key=${process.env.GOOGLE_MAPS_API_KEY}`);
+    const restaurants = data.results.filter(result =>
+      result.geometry &&
+      result.geometry.location &&
+      result.geometry.location.lat >= nycBounds.south &&
+      result.geometry.location.lat <= nycBounds.north &&
+      result.geometry.location.lng >= nycBounds.west &&
+      result.geometry.location.lng <= nycBounds.east
+    ).map(result => ({
+      name: result.name,
+      address: result.vicinity,
+      location: {
+        lat: result.geometry.location.lat,
+        lng: result.geometry.location.lng
+      },
+      placeId: result.place_id
+    }));
     res.json(restaurants);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).send(error.message);
   }
 });
 
 router.get('/:placeId', async (req, res) => {
   try {
-    const { placeId } = req.params;
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,formatted_address,formatted_phone_number,website,photo&key=${apiKey}`);
-    const result = response.data.result;
+    const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${req.params.placeId}&fields=name,formatted_address,geometry,formatted_phone_number,opening_hours,types,website&key=${process.env.GOOGLE_MAPS_API_KEY}`);
     const restaurant = {
-      name: result.name,
-      address: result.formatted_address,
-      phoneNumber: result.formatted_phone_number,
-      website: result.website,
-      photoUrl: result.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=${apiKey}` : null,
+      name: data.result.name,
+      address: data.result.formatted_address,
+      location: {
+        lat: data.result.geometry.location.lat,
+        lng: data.result.geometry.location.lng
+      },
+      vicinity: data.result.vicinity,
+      placeId: data.result.place_id,
+      formattedPhoneNumber: data.result.formatted_phone_number,
+      openingHours: data.result.opening_hours,
+      types: data.result.types,
+      website: data.result.website
     };
     res.json(restaurant);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).send(error.message);
   }
 });
 
