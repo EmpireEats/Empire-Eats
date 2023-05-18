c9eec01d97010d477c79bc809ff1360aa8b64638
reginar-kim <kimregina.r@gmail.com>
2023-05-17 23:41:01 -0400

    More style update still need to fix photos that are now gone

--- client/components/Leaderboard/Feed.js
+++ client/components/Leaderboard/Feed.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchFeed } from "../../redux/actions/feedActions";
 import { Link } from "react-router-dom";
+import "../../../public/styles/feed.css";
 
 const Feed = () => {
   const dispatch = useDispatch();
   const feed = useSelector((state) => state.feed.feed);
   const loading = useSelector((state) => state.feed.loading);
   const error = useSelector((state) => state.feed.error);
-
 
   useEffect(() => {
     dispatch(fetchFeed());
@@ -23,30 +23,34 @@ const Feed = () => {
   }
 
   return (
-    <div>
-      <div className="leader">
+    <div className="feed-container">
       <Link to="/home/leaderboard">
-        <button>LEADERBOARD</button>
+        <button>Leaderboard</button>
       </Link>
       <Link to="/home/feed">
         <button>Feed</button>
       </Link>
-      <ul>
+      <ul className="feed-list"> {/* Add a class to the ul element */}
         {feed.map((review, index) => (
           <li key={index}>
-            <img src={review.image} alt={`Review ${index}`} />
+            <img
+              src={review.pictureUrl}
+              alt={review.username}
+              style={{ width: "500px", height: "500px" }}
+            />
             <div>{review.name}</div>
-            <div> {review.address}</div>
-            <div>{review.previewText}-</div>
-            {/* <div>Reviewed by: {review.username}</div> */}
-            <div>Reviewed by: <Link to={`/users/${review.username}`}>{review.username}</Link></div>
+            <div>{review.address}</div>
+            <div>{review.previewText}</div>
+            <div>
+              Reviewed by:{" "}
+              <Link to={`/users/${review.username}`}>{review.username}</Link>
+            </div>
             <div>Posted on: {review.createdAt}</div>
           </li>
         ))}
       </ul>
-    </div></div>
+    </div>
   );
 };
 
 export default Feed;
-
