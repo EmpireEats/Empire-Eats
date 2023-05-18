dd708152af50f2a917ac6f804599d5cf4b19f16e
Stefani Chang <stefymerayja@gmail.com>
2023-05-18 13:45:45 -0400

    fixed some bugs in restaurant reviews

--- client/components/Reviews/ReviewPicsForPlace.js
+++ client/components/Reviews/ReviewPicsForPlace.js
@@ -4,34 +4,39 @@ import { useParams } from 'react-router-dom';
 import { fetchReviewsByPlaceAsync } from '../../redux/actions/reviewActions';
 import '../../../public/styles/weOutside.css'
 
+
 const ReviewPicsForPlace = () => {
   const dispatch = useDispatch();
-  const reviews = useSelector((state) => state.review.allReviews);
   const { placeId } = useParams();
-  const [page, setPage] = useState(1);
+  const reviews = useSelector(state => state.review.allReviews);
+  const [restaurantName, setRestaurantName] = useState("");
+
+  // useEffect(() => {
+  //   dispatch(fetchReviewsByPlaceAsync({ placeId, page: 1 }));
+  // }, [dispatch, placeId]);
 
   useEffect(() => {
-    dispatch(fetchReviewsByPlaceAsync({ placeId, page }));
-  }, [dispatch, placeId, page]);
-
-  const handleScroll = (event) => {
-    const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
-    if (scrollHeight - scrollTop === clientHeight) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
+    dispatch(fetchReviewsByPlaceAsync({ placeId, page: 1 }))
+      .then((result) => {
+        if (result.payload && result.payload.reviews.length > 0) {
+          setRestaurantName(result.payload.reviews[0].name);
+        }
+      });
+  }, [dispatch, placeId]);
 
   return (
-    <div className="review-image-container" onScroll={handleScroll}>
-      {reviews.map((review, index) => (
-        review.image && (
-          <div key={`${review.id}-${index}`} className="restaurant-review">
-            <img className="review-image" src={review.image} alt="Review" />
+    <div className="review-component">
+      <h2>Reviews for {restaurantName}</h2>
+      <div className="review-grid">
+        {reviews.map((review, index) => (
+          <div className="review-item" key={index}>
+            <img src={review.image} alt="Review"/>
           </div>
-        )
-      ))}
+        ))}
+      </div>
     </div>
-  );  
+  );
+  
 };
 
 export default ReviewPicsForPlace;
