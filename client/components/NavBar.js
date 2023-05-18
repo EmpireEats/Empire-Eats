bf8b539e03c949687d4a1d39b061d92d32fc5dd6
Stefani Chang <stefymerayja@gmail.com>
2023-05-18 11:18:05 -0400

    removed nav bar logic

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -3,35 +3,35 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { logout } from '../redux/actions/authActions';
-import Modal from 'react-modal';
-import { getGeolocation } from '../redux/actions/restaurantActions';
-import '../../public/styles/weOutside.css'
+// import Modal from 'react-modal';
+// import { getGeolocation } from '../redux/actions/restaurantActions';
+// import '../../public/styles/weOutside.css'
 
-Modal.setAppElement('#root');
+// Modal.setAppElement('#root');
 
 const NavBar = () => {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
-  const [modalMessage, setModalMessage] = useState('');
-  const [showModal, setShowModal] = useState(false);
+  // const [modalMessage, setModalMessage] = useState('');
+  // const [showModal, setShowModal] = useState(false);
 
-  const nycBounds = {
-    north: 40.917577,
-    south: 40.477399,
-    east: -73.700272,
-    west: -74.259090,
-  };
+  // const nycBounds = {
+  //   north: 40.917577,
+  //   south: 40.477399,
+  //   east: -73.700272,
+  //   west: -74.259090,
+  // };
 
-  const checkIfWithinNYC = (coords) => {
-    return (
-      coords.latitude >= nycBounds.south &&
-      coords.latitude <= nycBounds.north &&
-      coords.longitude >= nycBounds.west &&
-      coords.longitude <= nycBounds.east
-    );
-  };
+  // const checkIfWithinNYC = (coords) => {
+  //   return (
+  //     coords.latitude >= nycBounds.south &&
+  //     coords.latitude <= nycBounds.north &&
+  //     coords.longitude >= nycBounds.west &&
+  //     coords.longitude <= nycBounds.east
+  //   );
+  // };
 
   // // USE THIS FUNCTION AFTER DEMO DAY & REMOVE THE CURRENT ONE (THIS WILL PREVENT OUT OF BOUNDS & UNFOUND USERS FROM VISITING THE LINK)
   // const handleWeOutsideClick = async (event) => {
@@ -53,21 +53,21 @@ const NavBar = () => {
   
 
   // using this function for demo day so people can turn off location & have access to the link
-  const handleWeOutsideClick = async (event) => {
-    event.preventDefault();
-    try {
-      const coords = await getGeolocation();
-      const isWithinNYC = checkIfWithinNYC(coords);
-      if (!isWithinNYC) {
-        setModalMessage('You need to be in NYC if you want to be outside.');
-        setShowModal(true);
-      } else {
-        navigate('/restaurants');
-      }
-    } catch (error) {
-      navigate('/restaurants');
-    }
-  }; 
+  // const handleWeOutsideClick = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const coords = await getGeolocation();
+  //     const isWithinNYC = checkIfWithinNYC(coords);
+  //     if (!isWithinNYC) {
+  //       setModalMessage('You need to be in NYC if you want to be outside.');
+  //       setShowModal(true);
+  //     } else {
+  //       navigate('/restaurants');
+  //     }
+  //   } catch (error) {
+  //     navigate('/restaurants');
+  //   }
+  // }; 
 
   const closeModal = () => {
     setShowModal(false);
@@ -86,7 +86,9 @@ const NavBar = () => {
   return (
     <div className='nav-container'>
       <Link to='/leaderboard'>Home </Link>
-      <Link to='/restaurants' onClick={handleWeOutsideClick}>
+      <Link to='/restaurants' 
+        // onClick={handleWeOutsideClick}
+      >
         We Outside
       </Link>
       <Link to='/yerrr/now'>YERRR </Link>
@@ -106,7 +108,7 @@ const NavBar = () => {
       ) : (
         <Link to='/login'>Login</Link>
       )}
-      <Modal
+      {/* <Modal
         isOpen={showModal}
         onRequestClose={closeModal}
         className="weOutside-modal"
@@ -114,7 +116,7 @@ const NavBar = () => {
       >
         <h2>BING BONG</h2>
         <p>{modalMessage}</p>
-      </Modal>
+      </Modal> */}
     </div>
   );
 };
