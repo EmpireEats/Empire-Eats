da1b8127cb1325de93d6f3431e1bddd4184b9b45
reginar-kim <kimregina.r@gmail.com>
2023-05-18 02:58:46 -0400

    styled and updated feed and leaderboard

--- client/components/Leaderboard/Leaderboard.js
+++ client/components/Leaderboard/Leaderboard.js
@@ -21,39 +21,41 @@ const Leaderboard = () => {
     return <div>Error: {error}</div>;
   }
 
+  const limitedLeaderboard = leaderboard.slice(0, 20); // Limit the leaderboard array to 20 items
+
   return (
     <div>
-    <div className="leader">
-      <Link to="/home/leaderboard">
-        <button>LEADERBOARD</button>
-      </Link>
-      <Link to="/home/feed">
-        <button>FEED</button>
-      </Link>
+      <div className="leader">
+        <Link to="/home/leaderboard">
+          <button>LEADERBOARD</button>
+        </Link>
+        <Link to="/home/feed">
+          <button>FEED</button>
+        </Link>
       </div>
       <div className="leaderboard">
-      <table>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>User</th>
-            <th>Restaurant Visits</th>
-          </tr>
-        </thead>
-        <tbody>
-          {leaderboard.map((user, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              {/* <td>{user.name}</td> */}
-              <td>
-              <Link to={`/users/${user.username}`}>{user.name}</Link>
-              </td>
-              <td>{user.restaurantVisitCount}</td>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>User</th>
+              <th>Restaurant Visits</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {limitedLeaderboard.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                {/* <td>{user.name}</td> */}
+                <td>
+                  <Link to={`/users/${user.username}`}>{user.name}</Link>
+                </td>
+                <td>{user.restaurantVisitCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- server/db/models/Feed.js
+++ server/db/models/Feed.js
@@ -9,7 +9,7 @@ const getFeed = async () => {
     attributes: [
       "userId",
       "image",
-      [Sequelize.literal("substring(\"body\" from 1 for 50)"), "previewText"],
+      [Sequelize.literal("substring(\"body\" from 1 for 150)"), "previewText"],
       [Sequelize.col("review.createdAt"), "createdAt"], 
       "name",
       "address",
